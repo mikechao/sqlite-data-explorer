490da90a721fb3b5d80d113fe5b9c96073227fd8
mikechao <mike.chao.one@gmail.com>
2025-04-25 12:56:42 -0700

    Start SqliteMcpServer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import process from 'node:process';
 import { parseArgs } from 'node:util';
+import { SqliteMcpServer } from './server.js';
 
 const args = parseArgs({
   options: {
@@ -15,4 +16,23 @@ const args = parseArgs({
 if (!args.values['db-path']) {
   console.error('Error: --db-path argument is required.');
   process.exit(1);
+}
+
+const dbPath = args.values['db-path'] as string;
+const server = new SqliteMcpServer(dbPath);
+
+start()
+  .then(() => {
+    console.error('Server started successfully');
+  });
+
+async function start() {
+  try {
+    await server.ready;
+    await server.start();
+  }
+  catch (error) {
+    console.error(`Error starting server: ${error}`);
+    process.exit(1);
+  }
 }

bf9ca4fe63e95e27aa7ade145349785c314c7162
mikechao <mike.chao.one@gmail.com>
2025-04-25 13:08:52 -0700

    Added ListTablesTool

---
+++ src/tools/ListTablesTool.ts
@@ -0,0 +1,27 @@
+import type { Database } from 'sqlite';
+import type sqlite3 from 'sqlite3';
+import { z } from 'zod';
+import { BaseTool } from './BaseTool';
+
+const listTablesInputSchema = z.void();
+
+export class ListTablesTool extends BaseTool<typeof listTablesInputSchema, any> {
+  public readonly name = 'list_tables';
+  public readonly description = 'List all tables in the database.';
+  public readonly inputSchema = listTablesInputSchema;
+
+  constructor(private db: Database<sqlite3.Database, sqlite3.Statement>) {
+    super();
+  }
+
+  public async executeCore() {
+    const tables = await this.db.all('SELECT name FROM sqlite_master WHERE type="table"');
+    return {
+      content: tables.map(table => ({
+        type: 'text' as const,
+        text: table.name,
+      })),
+      isError: false,
+    };
+  }
+}
